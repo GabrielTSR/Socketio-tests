9f1ae914b2c84141f96e9cf850f4239feae184c0
GabrielTSR <gabrielalvesrocha22@gmail.com>
2022-04-11 10:54:32 -0300

    example finished

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const server = require('http').createServer(app); // criando o protocolo http
+const io = require('socket.io')(server); // criando o protocolo wss para o socket
+
+// Chat logic
+let messages = [];
+
+io.on('connection', (socket) => {
+    console.log(`Socket conectado: ${socket.id}`);
+    socket.emit('previousMessages', messages);
+
+    socket.on('sendMessage', (data) => {
+        messages.push(data);
+        socket.emit('receivedMessage', data);
+        socket.broadcast.emit('receivedMessage', data); // Envia mensagem para todos na conexao
+    });
+});
+
+// Iniciando server
+server.listen(3000);
